e1baeac0a5bfa18af54aeaf42ee695d702044f95
Sudeep Prasad <sudeep.rp@gmail.com>
2024-07-21 23:16:33 +0530

    mp3s done

--- line_to_speech.py
+++ line_to_speech.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-import random
+from time import sleep
 from merge_asky import only_alphabets
-from openai import OpenAI
+from openai import OpenAI, RateLimitError
 import os
 
 client = OpenAI()
@@ -11,18 +11,23 @@ def make_file_name_from_text(text):
    global counter_for_unique
    words = text.split()
    counter_for_unique += 1
-   unique_suffix = '{:02d}'.format(counter_for_unique)
-   return f'{only_alphabets(words[0])}_{only_alphabets(words[-1])}_{unique_suffix}.mp3'
+   unique_prefix = '{:02d}'.format(counter_for_unique)
+   return f'{unique_prefix}_{only_alphabets(words[0])}_{only_alphabets(words[-1])}.mp3'
 
 
 def text_to_speech(line_to_convert, speech_dir_path):
   speech_file_name = make_file_name_from_text(line_to_convert)
   file_path = os.path.join(speech_dir_path, speech_file_name)
-  with client.audio.speech.with_streaming_response.create(
-    model="tts-1-hd",
-    voice="onyx",
-    input=line_to_convert
-  ) as response:
-      response.stream_to_file(file_path)
-      print(f'wrote to {file_path}')
-      return speech_file_name
+  for i in range(3):
+    try:
+      with client.audio.speech.with_streaming_response.create(
+        model="tts-1-hd",
+        voice="onyx",
+        input=line_to_convert
+      ) as response:
+          response.stream_to_file(file_path)
+          print(f'wrote to {file_path}')
+          return speech_file_name
+    except RateLimitError:
+      print('rate limit hit! waiting 30s...')
+      sleep(30)
